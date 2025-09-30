c2c593bd6cc40d0c29c71f9069102c3ded8a208c
Nishaan-commits <mohammadnishaan24@gmail.com>
2025-09-30 14:06:27 +0530

    Initial commit

---
+++ Linear Regression/LR.py
@@ -0,0 +1,14 @@
+# %%
+from urllib.request import urlretrieve
+from IPython.display import display
+import pandas as pd
+
+
+
+
+medical_charges_url = 'https://raw.githubusercontent.com/JovianML/opendatasets/master/data/medical-charges.csv'
+urlretrieve(medical_charges_url, 'medical.csv')
+
+medical_df  = pd.read_csv('medical.csv')
+display(medical_df) 
+# %%
